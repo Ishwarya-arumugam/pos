74d758cb76c359d98d0b686fc210171251274c68
Ishwarya-arumugam <ishwaryaa44@gmail.com>
2016-06-29 10:20:46 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("positive number");
+}
+else
+{
+printf("negative number");
+}
+getch();
+}
